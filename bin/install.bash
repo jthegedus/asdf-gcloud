c1eb10676568560bec0d0c0772fec5f02c626055
jthegedus <james@jthegedus.com>
2020-02-29 20:56:52 +1100

    feat: init asdf plugin commands

---
+++ bin/install.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+install() {
+    printf "Install specified GCloud CLI version"
+}
+
+install
---
+++ bin/list-all.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+list_all() {
+    printf "Listing all GCloud CLI versions"
+}
+
+list_all
