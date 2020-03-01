e6cbf462114e723bb3c5146e70cdc24ce5e05f4a
jthegedus <james@jthegedus.com>
2020-03-01 19:56:36 +1100

    feat: list all gcloud sdk versions

--- bin/list-all.bash
+++ bin/list-all.bash
@@ -2,8 +2,20 @@
 
 set -euo pipefail
 
-list_all() {
-    printf "Listing all GCloud CLI versions"
+bucket_name=cloud-sdk-release
+prefix=google-cloud-sdk
+declare -a page_tokens=("" "Cihnb29nbGUtY2xvdWQtc2RrLTIwNy4wLjAtd2luZG93cy14ODYuemlw")
+
+function fetch() {
+    curl --silent GET "https://storage.googleapis.com/storage/v1/b/${bucket_name}/o?pageToken=${1}&prefix=${prefix}" |
+        grep -Eo "id.*google-cloud-sdk-[0-9]+\.[0-9]+\.[0-9]+-(linux)-x86_64" |
+        grep -Eo "[0-9]+\.[0-9]+\.[0-9]+"
 }
 
-list_all
+function list_all() {
+    for i in "${page_tokens[@]}"; do
+        fetch "$i"
+    done
+}
+
+list_all | sort -V
