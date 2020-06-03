811b4d474d03e0959fca6ab04b91809f84480647
James Hegedus <james@jthegedus.com>
2020-06-03 16:27:23 +1000

    fix: convert indent to tabs

--- lib/helpers.bash
+++ lib/helpers.bash
@@ -3,9 +3,9 @@
 set -euo pipefail
 
 function construct_gcs_object() {
-    local install_version="${1}"
-    local os_name="${2}"
-    local os_architecture="${3}"
+	local install_version="${1}"
+	local os_name="${2}"
+	local os_architecture="${3}"
 
-    echo "google-cloud-sdk-${install_version}-${os_name}-${os_architecture}.tar.gz"
+	echo "google-cloud-sdk-${install_version}-${os_name}-${os_architecture}.tar.gz"
 }
--- lib/utils.bash
+++ lib/utils.bash
@@ -4,79 +4,79 @@ set -euo pipefail
 
 # START Logging
 function log_failure_and_exit() {
-    printf "🚨  %s\\n" "${@}"
-    exit 1
+	printf "🚨  %s\\n" "${@}"
+	exit 1
 }
 
 function log_failure() {
-    printf "🚨  %s\\n" "${@}"
+	printf "🚨  %s\\n" "${@}"
 }
 
 function log_info() {
-    printf "%s\\n" "${@}"
+	printf "%s\\n" "${@}"
 }
 
 function log_success() {
-    printf "✅  %s\\n" "${@}"
+	printf "✅  %s\\n" "${@}"
 }
 
 function log_warning() {
-    printf "⚠️  %s\\n" "${@}"
+	printf "⚠️  %s\\n" "${@}"
 }
 # END Logging
 
 function check_dependencies() {
-    local dependencies_file="${1}"
-    local failure_type="${2}" # should be "warning" or "failure"
-    declare -a missing_dependencies=()
+	local dependencies_file="${1}"
+	local failure_type="${2}" # should be "warning" or "failure"
+	declare -a missing_dependencies=()
 
-    # loop over file of line separated list of dependencies required by this tool
-    while IFS="" read -r p || [ -n "${p}" ]; do
-        if [ ! "$(command -v "${p}")" ]; then
-            missing_dependencies+=("${p}")
-        fi
-    done <"${dependencies_file}"
+	# loop over file of line separated list of dependencies required by this tool
+	while IFS="" read -r p || [ -n "${p}" ]; do
+		if [ ! "$(command -v "${p}")" ]; then
+			missing_dependencies+=("${p}")
+		fi
+	done <"${dependencies_file}"
 
-    if [ "${#missing_dependencies[@]}" -ne 0 ]; then
-        if [ "${failure_type}" == "warning" ]; then
-            log_warning "Missing dependencies! These are hard requirements to install $(get_plugin_name)."
-            log_warning "${missing_dependencies[@]}"
-            log_info "You should install the listed dependencies before continuing."
-        else
-            log_failure "Missing dependencies! These are hard requirements to install $(get_plugin_name)."
-            log_failure_and_exit "${missing_dependencies[@]}"
-        fi
-    else
-        log_success "All dependencies found on system!"
-    fi
+	if [ "${#missing_dependencies[@]}" -ne 0 ]; then
+		if [ "${failure_type}" == "warning" ]; then
+			log_warning "Missing dependencies! These are hard requirements to install $(get_plugin_name)."
+			log_warning "${missing_dependencies[@]}"
+			log_info "You should install the listed dependencies before continuing."
+		else
+			log_failure "Missing dependencies! These are hard requirements to install $(get_plugin_name)."
+			log_failure_and_exit "${missing_dependencies[@]}"
+		fi
+	else
+		log_success "All dependencies found on system!"
+	fi
 }
 
 function get_os_architecture() {
-    local architecture
-    if [[ $(getconf LONG_BIT) == "64" ]]; then
-        architecture="x86_64"
-    else
-        architecture="x86"
-    fi
-    echo "${architecture}"
+	local architecture
+	if [[ $(getconf LONG_BIT) == "64" ]]; then
+		architecture="x86_64"
+	else
+		architecture="x86"
+	fi
+	echo "${architecture}"
 }
 
 function get_os_name() {
-    local os_name
-    case $(uname -s) in
-    Linux*)
-        os_name="linux"
-        ;;
-    Darwin*)
-        os_name="darwin"
-        ;;
-    *)
-        log_failure_and_exit "Script only supports macOS and Ubuntu"
-        ;;
-    esac
-    echo "${os_name}"
+	local os_name
+	case $(uname -s) in
+	Linux*)
+		os_name="linux"
+		;;
+	Darwin*)
+		os_name="darwin"
+		;;
+	*)
+		log_failure_and_exit "Script only supports macOS and Ubuntu"
+		;;
+	esac
+	echo "${os_name}"
 }
 
 get_plugin_name() {
-    basename "$(dirname "$(dirname "$0")")"
+	basename "$(dirname "$(dirname "$0")")"
 }
