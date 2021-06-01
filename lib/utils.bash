b3a79d758f10fe98b5d3164cf24f54758a606b0c
jthegedus <jthegedus@hey.com>
2021-06-01 18:59:38 +1000

    chore: update sort_versions to latest from source (ruby-vuild)

--- lib/utils.bash
+++ lib/utils.bash
@@ -82,7 +82,7 @@ get_plugin_name() {
 }
 
 function sort_versions() {
-	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
+	sed 'h; s/[-]/./g; s/.p\([[:digit:]]\)/.z.\1/; s/$/.z/; G; s/\n/ /' |
 		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n |
 		awk '{print $2}'
 }
