ff3d859dd7bbf9d18e48c5fdb24ce559086e2234
jthegedus <jthegedus@hey.com>
2021-06-01 19:47:49 +1000

    fix: sort keys properly

--- lib/utils.bash
+++ lib/utils.bash
@@ -83,6 +83,6 @@ get_plugin_name() {
 
 function sort_versions() {
 	sed 'h; s/[-]/./g; s/.p\([[:digit:]]\)/.z.\1/; s/$/.z/; G; s/\n/ /' |
-		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n |
+		LC_ALL=C sort -t. -k 1,1n -k 2,2n -k 3,3n |
 		awk '{print $2}'
 }
